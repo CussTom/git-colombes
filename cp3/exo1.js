e8bb8730056d9492edf76628a275cde934e62313
CussTom <thomas.cusse@gmail.com>
2021-01-08 15:33:54 +0100

    08012021

---
+++ cp3/exo1.js
@@ -0,0 +1,55 @@
+// Branche l'événement LOAD à WINDOW (DOM-2)
+
+window.addEventListener(
+    'load',
+    function(){
+        fillTable();
+    },
+    false
+);
+
+// Ou bien en DOM-0
+window.onload = function(){
+    fillTable();
+};
+
+
+// Fonction qui remplit le tableau HTML avec un array
+
+function fillTable(){
+    // Vide le TBODY
+    document.getElementById('tblBody').innerHTML = '';
+    // Pour chaque membre de l'array MEMBERS
+    let oRow, oCell;
+    for(let i=0; i<members.length; i++){
+        // Création du TR
+        oRow = document.createElement('tr');
+        // Création de la 1ère TD
+        oCell = document.createElement('td');
+        oCell.textContent = members[i].fname;
+        oRow.appendChild(oCell);
+         // Création de la 2nd TD
+        oCell = document.createElement('td');
+        oCell.textContent = members[i].age;
+        oRow.appendChild(oCell);
+          // Création de la 3e TD
+        oCell = document.createElement('td');
+        if(members[i].married){
+            
+        // DEUX SOLUTIONS :
+            // Avec IF
+            if(members[i].sex === "F"){
+                oCell.textContent = "Mariée";
+            }else{
+                oCell.textContent = "Marié";
+            }
+            // Avec TERNAIRE
+            oCell.textContent = (members[i].sex === 'F'? "Mariée":"Marié");
+        } else{
+            oCell.textContent = "Célibataire";
+        }
+        oRow.appendChild(oCell)
+        // rattache la TR au TBODY
+        document.getElementById('tblBody').appendChild(oRow);
+    }
+}
